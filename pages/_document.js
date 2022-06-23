14ba369263e5774e2537a59ce5adf55f80ff8be8
kaopcy <kaonaka@hotmail.co.th>
2022-06-23 13:25:33 +0700

    :lipstick: update font, style

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,18 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
                         rel="stylesheet"
                     ></link>
-
-                    <link href='https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css' rel='stylesheet' />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                        rel="stylesheet"
+                    ></link>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+                        rel="stylesheet"
+                    ></link>
+                    <link
+                        href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <div id="portal"></div>
                 <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { ScaleControl } = require("mapbox-gl");
+
 module.exports = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
@@ -8,6 +10,7 @@ module.exports = {
             fontFamily: {
                 kanit: ["kanit"],
                 sarabun: ["sarabun"],
+                notoSansThai: ["noto sans thai"],
             },
             fontSize: {
                 xxs: "10px",
@@ -16,19 +19,43 @@ module.exports = {
             colors: {
                 text: {
                     lightest: "#DFDFDF",
+                    lighterr: "#C2C2C2",
                     lighter: "#8B8B8B",
-                    DEFAULT: "#4f4f4f",
+                    // DEFAULT: "#4f4f4f",
+                    DEFAULT: "#4D4D4D",
                     dark: "#7F7F7F",
                 },
                 primary: {
+                    lighter: "#9AD7FF",
                     light: "#5AD7FF",
                     DEFAULT: "#5ABDFF",
                 },
             },
-            animation:{
-                'bounce-once': 'bounce 2s linear'
-            }
+            animation: {
+                "bounce-once": "bounce 2s linear",
+                "gps-pulse" : "gps-pulse 2s infinite"
+            },
+            boxShadow: {
+                big: "0px 1px 6px  rgba(0, 0, 0, 0.16)",
+                icon: "2px 2px 10px  rgba(0, 0, 0, 0.16)",
+            },
+            keyframes: {
+                "gps-pulse": {
+                    "0%": {
+                        transform: "scale(0.95)",
+                        boxShadow: "0 0 0 0 rgba(245,66,66, 0.7)",
+                    },
+                    "70%": {
+                        transform: "scale(1)",
+                        boxShadow: "0 0 0 25px rgba(245,66,66, 0)",
+                    },
+                    "100%": {
+                        transform: "scale(0.95)",
+                        boxShadow: "0 0 0 0 rgba(245,66,66, 0)",
+                    },
+                },
+            },
         },
     },
-    plugins: [],
+    plugins: [require("tailwindcss-container-query")],
 };
