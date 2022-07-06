637db57d21c599d2ece8170b012ad3204e7c4729
kaopcy <kaonaka@hotmail.co.th>
2022-07-06 22:00:01 +0700

    :bug: fix bug'

--- pages/restaurant/index.js
+++ pages/restaurant/index.js
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import groq from "groq";
+import Head from "next/head";
+import { useRouter } from 'next/router'
 
-const Travel = () => {
-  return (
-      <div className="">
-          
-      </div>
-  )
+// import configs
+import { getClient } from "../../lib/sanity.server";
+
+// import contexts
+import { MapContextProvider } from "../../context/MainTravel/MapContext";
+import { PostsContextProvider } from "../../context/MainTravel/PostContext";
+
+// imports hooks
+import useIsTouchDevice from "../../composables/useIsTouchDevice";
+
+// import components
+import Posts from "../../components/MainTravel/Posts";
+import Map from "../../components/MainTravel/Map";
+
+const Travel = ({ posts }) => {
+    // const router = useRouter()
+    // const { query } = router
+    // useEffect(()=> {
+    //     if(query.map !== undefined) return 
+    //     router.push('?map',undefined , { shallow: true })
+    //     console.log('query' , query);
+    // },[])
+    
+
+    const isTouch = useIsTouchDevice();
+    return (
+        <MapContextProvider>
+            <PostsContextProvider initPosts={posts}>
+                <main
+                    className={`relative flex   w-full flex-col overflow-hidden  ${
+                        isTouch ? "h-[calc(100vh-70px)]" : "h-screen"
+                    }`}
+                >
+                    <Head>
+                        <title>ร้านอาหารในชลบุรี</title>
+                        <meta
+                            name="description"
+                            content="ค้นหาร้านอาหารชื่อดังมากมายในชลบุรี รวมไปถึงแสดงแผนที่และเส้นทางในการเดินทาง"
+                        />
+                    </Head>
+                    <div className="h-[70px] w-full shrink-0 md:h-[100px]"></div>
+                    <div className="flex h-full w-full overflow-hidden">
+                        <Posts />
+                        <Map />
+                    </div>
+                </main>
+            </PostsContextProvider>
+        </MapContextProvider>
+    );
+};
+
+const postsQuery = groq`
+*[(_type == "pointOfInterest") && defined(slug.current)]{
+  amphoe-> { name },
+  tambon-> { name },
+  title,
+  slug,
+  coords,
+  placeID,
+  imageURL,
+  reviews,
+  star,
+}`;
+
+export async function getStaticProps({ params, preview = false }) {
+    const posts = await getClient(preview).fetch(postsQuery);
+    return {
+        props: {
+            posts: posts
+                .sort((a, b) => b.star - a.star)
+                .reduce((prev, cur) => {
+                    if (prev[cur.amphoe.name]) {
+                        return {
+                            ...prev,
+                            [cur.amphoe.name]: [
+                                ...prev[cur.amphoe.name],
+                                { ...cur },
+                            ],
+                        };
+                    }
+                    return { ...prev, [cur.amphoe.name]: [{ ...cur }] };
+                }, {}),
+        },
+    };
 }
 
-export default Travel
+export default Travel;
